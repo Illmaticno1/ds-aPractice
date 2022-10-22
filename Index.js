57b7bc417fb7a3c6312a8c8529ce88a4b48e94fb
Illmaticno1 <demetrius.robinson@icloud.com>
2022-10-22 16:30:51 +0000

    working on linkedList

--- Index.js
+++ Index.js
@@ -12,7 +12,18 @@ class LinkedList {
     this.tail = this.head
     this.length = 1
   }
-    push(value)
+    push(value) {
+      const newNode = new Node(value)
+      if (!this.head) {
+        this.head = newNode
+        this.tail = newNode
+      } else {
+        this.tail.next = newNode
+        this.tail = newNode
+      }
+      this.length++
+      return this
+    }
     unshift(value)
     insert(index, value)
 }
