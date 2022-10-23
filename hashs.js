52b38fa98f2dd03f11fe2bf2dd85d632ae38ae80
Illmaticno1 <Demetrius.robinson@icloud.com>
2022-10-23 19:19:33 -0400

    created a hashTable class

---
+++ hashs.js
@@ -0,0 +1,16 @@
+// hash tables
+// hash table in objects have a key value pair
+// the key has to be a string which has to be unique and is put in a hash generator which calculates the address in memory
+// hash is a one way operation which means you cant take the memory address and find the key or value
+// hashes are deterministic meaning the hash will always produce the same number for the memory address
+// collisions happen when separate keys are hashed and mapped to the same memory address
+// how to handle collisions (two most common ways are linear probing and separate chaining)
+// separate chaining - adding each item mapped to a single memory address to an array or to a linked list
+// linear probing - if multiple key value pairs are mapped to the same memory address it checks for the next available slot and maps it to that location (open addressing)
+// it is more optimal to have a more randomized distribution of items you add to the hash table if you have a prime number of items
+
+class hashTable {
+  constructor(size = 7) {
+    this.dataMap = new Array(size);
+  }
+}
