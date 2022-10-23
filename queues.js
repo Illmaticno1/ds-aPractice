ce2118f8d33db1a4968347fb32427e83583832e2
Illmaticno1 <Demetrius.robinson@icloud.com>
2022-10-23 17:33:12 -0400

    added dequeue method

--- queues.js
+++ queues.js
@@ -18,7 +18,37 @@ class Queue {
     this.last = newNode;
     this.length = 1;
   }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  dequeue() {
+    if (this.length === 0) return undefined;
+    let temp = this.first;
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
 }
 
-let myQueue = new Queue(4);
+let myQueue = new Queue(11);
+myQueue.enqueue(3);
+myQueue.enqueue(23);
+myQueue.enqueue(7);
+console.log(myQueue.dequeue().value);
+
 console.log(myQueue);
